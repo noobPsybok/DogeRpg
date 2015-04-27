2ab8fab0b574b5247a7c03d8a75a983526c40145
intrepidSchooner <intrepidschooner@outlook.com>
2015-04-27 14:35:14 -0500

    idk

--- Assets/scripts/StateManager.cs
+++ Assets/scripts/StateManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FollowCam : MonoBehaviour {
+public class StateManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
